839766afee856d5ee6b5dc416cb6e863ff5b9bf3
Alexander-Sultanaev <100706479+Alexander-Sultanaev@users.noreply.github.com>
2022-12-21 14:47:15 +0500

    Добавлена joi валидация к контролеру users

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,32 @@
 const userRoutes = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 const {
   getUsers, getUser, updateInfoUser, updateAvatarUser, getUserInfo,
 } = require('../controllers/users');
 
 userRoutes.get('/', getUsers);
-userRoutes.get('/:userId', getUser);
 userRoutes.get('/:me', getUserInfo);
-userRoutes.patch('/me', updateInfoUser);
-userRoutes.patch('/me/avatar', updateAvatarUser);
+userRoutes.get('/:userId', celebrate({
+  body: Joi.object().keys({
+    userId: Joi.string().hex().required().length(24),
+  }),
+}), getUser);
+userRoutes.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
+  }),
+}), updateInfoUser);
+userRoutes.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().required().custom((value, helpers) => {
+      if (/^(http|https):\/\/\S+(?:jpg|jpeg|png)$/.test(value)) {
+        return value;
+      }
+      return helpers.message('Передан некорректный URL-адрес');
+    }),
+
+  }),
+}), updateAvatarUser);
 
 module.exports = userRoutes;
