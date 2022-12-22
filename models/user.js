8b1945d7980e2176b41d2cb042b7142aabf86c13
Alexander-Sultanaev <100706479+Alexander-Sultanaev@users.noreply.github.com>
2022-12-22 12:03:32 +0500

    Тест фикс

--- models/user.js
+++ models/user.js
@@ -4,19 +4,26 @@ const isEmail = require('validator/lib/isEmail');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    require: false,
     minLength: 2,
     maxLength: 30,
+    default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
-    required: true,
+    require: false,
     minLength: 2,
     maxLength: 30,
+    default: 'Исследователь',
   },
   avatar: {
     type: String,
-    required: true,
+    require: false,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    validate: {
+      validator: (value) => /^(http|https):\/\/[^ "]+$/.test(value),
+      message: 'Неправильны формат ссылки!',
+    },
   },
   email: {
     type: String,
