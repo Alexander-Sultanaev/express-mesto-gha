a9dd39ab781d1281232e271165451e87b10a1f93
Alexander-Sultanaev <100706479+Alexander-Sultanaev@users.noreply.github.com>
2022-12-22 11:25:13 +0500

    Фикс автотест

--- routes/users.js
+++ routes/users.js
@@ -1,32 +1,12 @@
 const userRoutes = require('express').Router();
-const { celebrate, Joi } = require('celebrate');
 const {
-  getUsers, getUser, updateInfoUser, updateAvatarUser, getUserInfo,
+  getUsers, getUser, updateInfoUser, updateAvatarUser,
 } = require('../controllers/users');
 
 userRoutes.get('/', getUsers);
-userRoutes.get('/:me', getUserInfo);
-userRoutes.get('/:userId', celebrate({
-  body: Joi.object().keys({
-    userId: Joi.string().hex().required().length(24),
-  }),
-}), getUser);
-userRoutes.patch('/me', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    about: Joi.string().required().min(2).max(30),
-  }),
-}), updateInfoUser);
-userRoutes.patch('/me/avatar', celebrate({
-  body: Joi.object().keys({
-    avatar: Joi.string().required().custom((value, helpers) => {
-      if (/^(http|https):\/\/\S+(?:jpg|jpeg|png)$/.test(value)) {
-        return value;
-      }
-      return helpers.message('Передан некорректный URL-адрес');
-    }),
 
-  }),
-}), updateAvatarUser);
+userRoutes.get('/:userId', getUser);
+userRoutes.patch('/me', updateInfoUser);
+userRoutes.patch('/me/avatar', updateAvatarUser);
 
 module.exports = userRoutes;
